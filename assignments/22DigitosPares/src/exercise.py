369fd520cf61846aa88cc85688cdc1bed37ffdd9
RamsesEscoto <89435918+RamsesEscoto@users.noreply.github.com>
2021-08-23 23:18:03 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+      digito=int(input("Dame el número: "))
+    par=0
+    if digito>-10000 and digito<=-1000 or digito>=1000 and digito<10000:
+        while (digito>0):
+            if digito%2==0:
+                par=par+1
+            else: 
+                par=par+0
+            digitos=digito//10
+        print("El número de dígitos pares es: "+ str(par))
     pass
 
 
