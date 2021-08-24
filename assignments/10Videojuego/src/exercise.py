6ddbafb777b6b2855f0c9776dc69e07714841859
RamsesEscoto <89435918+RamsesEscoto@users.noreply.github.com>
2021-08-23 23:07:05 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    nuevo=int(input("inserta el numero de juegos nuevos: "))
+   usado=int(input("inserta el numero de juegos usados: ")) 
+   nuevos= nuevo*1000
+   usados= usado*350
+   total= nuevos+usados
+   print("El total de la compra es: "+str(total))
    pass
 
 
