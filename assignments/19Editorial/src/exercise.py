ee40d1027b9cc15bb4b65725824dc8b6934b4960
RamsesEscoto <89435918+RamsesEscoto@users.noreply.github.com>
2021-08-23 23:13:39 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+       palabrast=int(input("ingresa número de palabras: "))
+    pags=(math.ceil(palabrast/475))
+    total=pags*60
+    descuento=total*.10
+    totals=total-descuento
+    totals=round(totals,1)
     pass
 
 
